8af3d727faf95d34f1815de4c7fcec1b3d6e7253
Md. Tarikul Islam <141022752+MRT4R1KUL1SL4M@users.noreply.github.com>
2025-08-30 01:13:06 +0600

    Add files via upload

---
+++ MMAL/STRINGCMP.asm
@@ -0,0 +1,32 @@
+
+; Comparision of two string
+
+.data
+    string1 db 'DIU'    
+    len equ ($-string1)
+    string2 db 'DIU'
+    output db 'No','$'
+    success_output db 'Yes','$'
+
+.code
+    lea si, string1
+    lea di, string2
+    mov cx, len
+    repe cmpsb
+    jne not_equal
+
+    lea dx, success_output
+    jmp print
+
+not_equal:
+    lea dx, output
+
+print:
+    mov ah, 09h
+    int 21h
+    ret
+
+
+
+
+
