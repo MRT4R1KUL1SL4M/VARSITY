974569b84e1270ca1369d6796c99e153dfc7b78d
Md. Tarikul Islam <141022752+MRT4R1KUL1SL4M@users.noreply.github.com>
2025-08-30 02:21:11 +0600

    Update 7. STRINGCMP.asm

--- MMAL/7. STRINGCMP.asm
+++ MMAL/7. STRINGCMP.asm
@@ -1,31 +1,34 @@
-
-; Comparision of two string
+org 100h
 
 .data
     string1 db 'DIU'    
     len equ ($-string1)
     string2 db 'DIU'
-    output db 'No','$'
-    success_output db 'Yes','$'
-
+    output db 'a$'
+    
 .code
     lea si, string1
     lea di, string2
+
+    
     mov cx, len
+
+    
     repe cmpsb
     jne not_equal
-
-    lea dx, success_output
-    jmp print
-
-not_equal:
-    lea dx, output
-
-print:
+    
+    mov output, 'Y'
+    
+    jmp end
+    
+    not_equal:
+    mov output, 'N'
+    
+    end:
     mov ah, 09h
+    lea dx, output
     int 21h
-    ret
-
+        ret
 
 
 
